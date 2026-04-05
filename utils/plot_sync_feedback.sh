47cba0b4b157c6f077ad861d5491b4870c76e1be
igiannakas <ioannis@my-spot.co.uk>
2026-04-05 19:59:33 +0100

    Update plot_sync_feedback.sh

--- utils/plot_sync_feedback.sh
+++ utils/plot_sync_feedback.sh
@@ -21,4 +21,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 MMU_DIR="${SCRIPT_DIR}/../extras/mmu"
 export PYTHONPATH="${MMU_DIR}:${PYTHONPATH}"
 
+<<<<<<< Updated upstream
 python "${SCRIPT_DIR}/sync_feedback_sim.py" --plot "$log"
+=======
+python3 "${SCRIPT_DIR}/sync_feedback_sim.py" --plot "$log"
+>>>>>>> Stashed changes
